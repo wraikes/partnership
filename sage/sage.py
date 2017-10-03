3309637190233aa7a78204e843588e19ce0caa1e
William Raikes <wraikes@hotmail.com>
2017-10-03 15:12:26 -0400

    Modify sage code

--- sage/sage.py
+++ sage/sage.py
@@ -1,192 +1,167 @@
-import os, pandas as pd, numpy as np, re
+import os, pandas as pd, numpy as np, re, time
 from functools import reduce
 
 
 def main():
-    sage = '/home/wraikes/Dropbox/partnership/dmt/data/sage_not_final'
-    #sage = r'C:\Users\williamr.PDFK\Dropbox\partnership\dmt_temp\Sage (Mobile Phone) Data 8.31.17'
+    #sage = '/home/wraikes/Dropbox/partnership/dmt/data/sage_not_final'
+    sage = r'C:\Users\williamr.PDFK\Dropbox\partnership\dmt\data\sage_not_final'
+    file_to_write = r'H:\Documents\Python Scripts\dmt\partnership\merged_data'
     os.chdir(sage)
+    
     files_to_exclude = ['digital-marshmallow-status_8.31.17.csv',
                         'digital-marshmallow-appVersion_8.31.17.csv']
     end_string = '_8.31.17.csv'
-    merge_files(os.listdir(), files_to_exclude, end_string)
-
+    bl = 'baseline'
+    _21 = '21-day-assessment'
     
-def merge_files(directory, exclude, end_string):
+    common_df = clean_common_dfs(os.listdir(), files_to_exclude, end_string)
+    
+    #To target the behavior_choices_1 df
+    common_df = selected_recode(common_df)
+        
+    bart_att = [
+        [bl, 'BART0.25'],
+        [bl, 'BART250.00'],
+        [_21, 'BART0.25'],
+        [_21, 'BART250.00']       
+    ]
+    
+    bart_df = clean_df('digital-marshmallow-bart-v4_8.31.17.csv',
+                   end_string,
+                   bart_att,
+                   var_2='data.json.variable_label')
+    
+    delay_att = [
+        [bl, 'dd_time_6_month'],
+        [bl, 'dd_money_6_month'],
+        [bl, 'dd_money_1_month'],
+        [bl, 'dd_time_1_year'],
+        [_21, 'dd_time_6_month'],
+        [_21, 'dd_money_6_month'],
+        [_21, 'dd_money_1_month'],
+        [_21, 'dd_time_1_year']            
+    ]
+    
+    delay_df = clean_df('digital-marshmallow-delay_discounting_raw-v6_8.31.17.csv',
+                    end_string,
+                    delay_att,
+                    var_2='data.json.variableLabel')    
+    
+    delay_df = df_array_last_value(delay_df)
+    delay_df = df_split_array(delay_df)
+    
+    behave_4_att = [
+        [bl]
+    ]
+    
+    behave_4_df = clean_df('digital-marshmallow-behavior_choices_4_bl-v2_8.31.17.csv',
+                       end_string,
+                       behave_4_att)
+    
+    behave_4_df = selected_spread(behave_4_df)
+    
+    discount_att = [
+        [bl, 'pd_constant_money'],
+        [bl, 'pd_constant_probability'],
+        [_21, 'pd_constant_money'],
+        [_21, 'pd_constant_probability']
+    ]
+    
+    discount_df = clean_df('digital-marshmallow-discounting_raw-v2_8.31.17.csv',
+                      end_string,
+                      discount_att,
+                      var_2='data.json.variableLabel')
+    
+    evening_note_att = [
+        [bl]
+    ] 
+    
+    evening_note_df = clean_df('digital-marshmallow-evening_notification_time-v2_8.31.17.csv', 
+                           end_string,
+                           evening_note_att) 
+    
+    gonogo_att = [
+        [bl, 'go_no_go_stable_stimulus_active_task'],
+        [bl, 'go_no_go_variable_stimulus_active_task'],
+        [_21, 'go_no_go_stable_stimulus_active_task'],
+        [_21, 'go_no_go_variable_stimulus_active_task']
+    ]
+    
+    gonogo_df = clean_df('digital-marshmallow-goNoGo-v2_8.31.17.csv', 
+                     end_string,
+                     gonogo_att,
+                     var_2='data.json.variable_label')
+    
+    morning_note_att = [
+        [bl]
+    ]   
+    
+    morning_note_df = clean_df('digital-marshmallow-morning_notification_time-v3_8.31.17.csv',
+                          end_string,
+                          morning_note_att)
+    
+    pam_mult_att = [
+        [bl],
+        [_21]
+    ]
+     
+    pam_mult_df = clean_df('digital-marshmallow-pam_multiple-v2_8.31.17.csv',
+                      end_string,
+                      pam_mult_att)
+    
+    df = merge_dfs([common_df, 
+                        bart_df,
+                        delay_df,
+                        behave_4_df,
+                        discount_df,
+                        evening_note_df,
+                        gonogo_df,
+                        morning_note_df,
+                        pam_mult_df])  
+
+    df = timestamp_conv(df)
+    
+    os.chdir(file_to_write)
+    df.to_csv('FINAL_SAGE.csv', index=False)
+
+
+def merge_dfs(dfs):
+    df = reduce(lambda left, right: pd.merge(left, right, how = 'outer',
+                                             on='externalId'), 
+                dfs)
+    
+    return df
+
+
+def clean_common_dfs(directory, exclude_files, string):
     '''
-    Go through and process all files in directory.
-    If extra processing is needed, use a special function 'new_df_func'.
+    Go through and process all files in directory that don't need columns "spread".
+    If extra processing is needed, a different function will be used.
     Return a single dataframe, of all data merged.
     '''
     
     new_dfs = []
     
-    for file in directory:
-        name = create_df_name(file, end_string)
-        
-        if file not in exclude:
+    for file in directory: 
+        if file not in exclude_files:
+            df_name = create_df_name(file, string)
             df = pd.read_csv(file)
             df = remove_dupes(df)
-            if len(df.externalId) == len(df.externalId.unique()):
-                df = col_relabel(df, name)          
-                new_dfs.append(df)
+            if df_name in ('demographics-v2', 'generally_sem_diff_bl-v2'):
+                df = clean_df_dupe(df, 'yQ7pYy')
             
-            else:
-                df = new_df_func(df, name)
+            if len(df.externalId) == len(df.externalId.unique()):
+                df = col_relabel(df, df_name)
                 new_dfs.append(df)
     
-    final_df = reduce(lambda left, right: pd.merge(left, right, how = 'outer',
-                                                   on='externalId'), 
-                      new_dfs)
-    
-    return final_df
+    return merge_dfs(new_dfs)
 
 
-def new_df_func(df, df_name):
-    '''
-    Each dataframe is cleaned with special instructions based on the filters.
-    A cleaned dataframe is returned.
-    '''
+def create_df_name(string, end_string):
+    string = string.replace('digital-marshmallow-', '')
+    string = string.replace(end_string, '')
     
-    if df_name == 'bart-v4':
-        attributes = [
-            ['baseline', 'BART0.25'],
-            ['baseline', 'BART250.00'],
-            ['21-day-assessment', 'BART0.25'],
-            ['21-day-assessment', 'BART250.00']
-        ]
-                
-        df = spread_cols(df, df_name, attributes, 
-                         var_1='metadata.json.taskIdentifier', 
-                         var_2='data.json.variable_label')
-        
-    elif df_name == 'delay_discounting_raw-v6':
-        bl = 'baseline'
-        _21 = '21-day-assessment'
-        
-        attributes = [
-            [bl, 'dd_time_6_months'],
-            [bl, 'dd_money_6_month'],
-            [bl, 'dd_money_1_month'],
-            [bl, 'dd_time_1_year'],
-            [_21, 'dd_time_6_month'],
-            [_21, 'dd_money_6_month'],
-            [_21, 'dd_money_1_month'],
-            [_21, 'dd_time_1_year']            
-        ]
-        
-        df = spread_cols(df, df_name, attributes, 
-                         var_1='metadata.json.taskIdentifier',
-                         var_2='data.json.variableLabel')
-
-    elif df_name == 'demographics-v2' or df_name == 'generally_sem_diff_bl-v2':
-        indices = df.index[df.externalId == 'yQ7pYy']
-        df.drop(indices[-1], axis=0, inplace=True)
-
-    elif df_name == 'behavior_choices_4_bl-v2':
-        attributes = [
-            'baseline'        
-        ]
-        
-        df = spread_cols(df, df_name, attributes,
-                         var_1='metadata.json.taskIdentifier')
-        
-    elif df_name == 'discounting_raw-v2':
-        bl = 'baseline'
-        _21 = '21-day-assessment'
-
-        attributes = [
-            [bl, 'pd_constant_money'],
-            [bl, 'pd_constant_probabiliy'],
-            [_21, 'pd_constant_money'],
-            [_21, 'pd_constant_probability']
-        ]
-        
-        df = spread_cols(df, df_name, attributes,
-                         var_1='metadata.json.taskIdentifier',
-                         var_2='data.json.variableLabel')
-    
-    elif df_name == 'evening_notification_time-v2':
-        attributes = ['baseline']
-
-        df = spread_cols(df, df_name, attributes,
-                         var_1='metadata.json.taskIdentifier')
-                        
-    elif df_name == 'goNoGo-v2':
-        bl = 'baseline'
-        _21 = '21-day-assessment'
-
-        attributes = [
-            [bl, 'go_no_go_stable_stimulus_active_task'],
-            [bl, 'go_no_go_variable_stimulus_active_task'],
-            [_21, 'go_no_go_stable_stimulus_active_task'],
-            [_21, 'go_no_go_variable_stimulus_active_task']
-        ]
-        
-        indices = df.index[df.externalId == 'ksJM3Y']
-        df.drop(indices[-1], axis=0, inplace=True)
-        
-        df = spread_cols(df, df_name, attributes,
-                         var_1='metadata.json.taskIdentifier',
-                         var_2='data.json.variable_label')
-    
-    elif df_name == 'morning_notification_time-v3':
-        attributes = ['baseline']
-        
-        df = spread_cols(df, df_name, attributes,
-                         var_1='metadata.json.taskIdentifier')
-    
-    elif df_name == 'pam_multiple-v2':
-        attributes = [
-            'baseline',
-            '21-day-assessment'
-        ]
-        
-        df = spread_cols(df, df_name, attributes,
-                         var_1='metadata.json.taskIdentifier')        
-        
-    return df
-
-    
-def spread_cols(df, df_name, attributes, var_1, var_2=None):
-    '''
-    Merge the dataframes into one, with new columns.
-    '''
-    
-    dfs = []
-    
-    for att in attributes:
-        new_df = filter_df(df, att, var_1, var_2)
-        new_df = col_relabel(new_df, df_name, att)
-        dfs.append(new_df)
-    
-    new_df = reduce(lambda left, right: pd.merge(left, 
-                                                 right, 
-                                                 how = 'outer',
-                                                 on='externalId'),
-                    dfs)
-    
-    return new_df
-
-
-def filter_df(df, att, var_1, var_2=None):
-    '''
-    Filter a dataframe based off of var_1 and var_2 variables.
-    '''
-    
-    cols = df.columns
-    
-    new_df = pd.DataFrame(columns=cols)
-        
-    for ix, row in df.iterrows():
-        if var_2:
-            if row[var_1] == att[0] and row[var_2] == att[1]:
-                new_df = new_df.append(row, ignore_index=True)
-        else:
-            if row[var_1] == att[0]:
-                new_df = new_df.append(row, ignore_index=True)
-    
-    return new_df
+    return string 
 
 
 def col_relabel(df, prefix, att=None):
@@ -198,30 +173,26 @@ def col_relabel(df, prefix, att=None):
     new_cols = []
     col_re = re.compile('metadata.json.|data.json.')
     
-    
     for col in df.columns:
         new_col = re.sub(col_re, '', col)
-
+    
         if new_col != 'externalId':
             if att and len(att) == 2:
-                new_col = '{}_{}_{}_{}___{}'.format('SAGE', 
-                                                    prefix, 
-                                                    att[0], 
-                                                    att[1], 
-                                                    new_col
-                                                    )
+                new_col = 'SAGE_{}_{}_{}___{}'.format(prefix, 
+                                                      att[0], 
+                                                      att[1], 
+                                                      new_col
+                                                     )
             elif att and len(att) == 1:
-                new_col = '{}_{}_{}___{}'.format('SAGE', 
-                                                 prefix, 
-                                                 att[0], 
-                                                 new_col
-                                                 )                
+                new_col = 'SAGE_{}_{}___{}'.format(prefix, 
+                                                   att[0], 
+                                                   new_col
+                                                  )                
             
             else:
-                new_col = '{}_{}___{}'.format('SAGE', 
-                                              prefix, 
-                                              new_col
-                                              ) 
+                new_col = 'SAGE_{}___{}'.format(prefix, 
+                                                new_col
+                                                ) 
                                               
         new_col = new_col.replace(' ', '_')
         new_col = new_col.replace('-', '_')
@@ -231,14 +202,7 @@ def col_relabel(df, prefix, att=None):
     
     return df
 
-def create_df_name(string, end_string):
-    string = string.replace('digital-marshmallow-', '')
-    string = string.replace(end_string, '')
-    
-    return string        
-            
 
-#Functions for data processing
 def remove_dupes(df):
     
     test_users = [
@@ -252,7 +216,8 @@ def remove_dupes(df):
         'WbbNWM4RAF', 
         'D5bzYrfd8E', 
         'LJcmEFWp74', 
-        'ULoF3MM1nN'
+        'ULoF3MM1nN',
+        'G9Bbsg'
     ]
 
     diff_study = [
@@ -274,19 +239,127 @@ def remove_dupes(df):
         'mPYk2p',
         'mP3rbd'
     ]
-
-    
+  
     return df[~df.externalId.isin(test_users + diff_study)]
 
-test = main()
 
-#def dupe_check(df):
-#    return len(df.externalId) == len(df.externalId.unique()) and len(df.externalId) > 0
+def spread_cols(df, df_name, attributes, var_2):
+    '''
+    Merge the dataframes into one, with new columns.
+    '''
+    dfs = []
+    var_1 = 'metadata.json.taskIdentifier'
+    
+    for att in attributes:
+        cols = df.columns
+        new_df = pd.DataFrame(columns=cols)
+        
+        for ix, row in df.iterrows():
+            if var_2:
+                if row[var_1] == att[0] and row[var_2] == att[1]:
+                    new_df = new_df.append(row, ignore_index=True)
+            else:
+                if row[var_1] == att[0]:
+                    new_df = new_df.append(row, ignore_index=True)
+                    
+        new_df = col_relabel(new_df, df_name, att)
+        dfs.append(new_df)
+    
+    return merge_dfs(dfs)
 
 
-#test = merge_files(os.listdir(), files_to_exclude, end_string)
+def clean_df(file, string, attributes, var_2=None):
+    df_name, df = create_df_name(file, string), pd.read_csv(file)
+    df = remove_dupes(df)
+    
+    if df_name == 'goNoGo-v2':
+        df = clean_df_dupe(df, 'ksJM3Y')
+    
+    if df_name == 'behavior_choices_4_bl-v2':
+        df = clean_df_dupe(df, 'yQ7pYy')
+    
+    df = spread_cols(df, df_name, attributes, var_2)
+    
+    return df
 
 
+def clean_df_dupe(df, _id):
+   
+    indices = df.index[df.externalId == _id]
+    df.drop(indices[-1], axis=0, inplace=True)
+    
+    return df
 
 
+def timestamp_conv(df):
+    
+    for col in df.columns:
+        if any([col.endswith(x) for x in ('createdOn', 'startDate', 'endDate')]):
+            df[col] = df[col][df[col].notnull()].apply(lambda x: time.strftime("%m.%d.%Y %H:%M - %Z", time.localtime(x / 1000)))
+    
+    return df
+
+
+def array_split(value, i):
+    if pd.notnull(value):
+        return list(map(float, value[1:-1].split(',')))[i]
+    else:
+        return value
+
+    
+def df_array_last_value(df):
+    new_df = df.copy()
+
+    for col in df.columns:
+        if 'nowArray' in col:
+            new_col = col + '_last'
+            new_df[new_col] = df[col].apply(lambda x: array_split(x, -1))
+    
+    return new_df
+
+
+def df_split_array(df):
+    new_df = df.copy()
+    
+    for col in df.columns:
+        if all([x in col for x in ('times', 'delay', 'SAGE')]):
+            for i in range(6):
+                new_col = col + '_v' + str(i)
+                new_df[new_col] = df[col].apply(lambda x: array_split(x, i))
+                
+    return new_df
+
+
+def selected_spread(df):
+    #Note: codebook is incorrect. 'gamble' vs. 'gambling'.
+    terms = ['junkfood', 'gambling', 'smoke', 'mobile', 'overeat', 'mast', 'cut', 'smedia',
+             'alcohol', 'porn', 'hair', 'gaming', 'sitting', 'spend', 'temper', 'othermore']
+
+    for term in terms:
+        col_name = 'SAGE_behavior_choices_4_bl_v2_baseline___selected' + '_' + term
+        df[col_name] = np.where(df['SAGE_behavior_choices_4_bl_v2_baseline___selected'].str.contains(term), 1, 0)
+        
+    return df
+
+
+def recode(value):
+    
+    terms = ['junkfood', 'gambling', 'smoke', 'mobile', 'overeat', 'mast', 'cut', 'smedia',
+             'alcohol', 'porn', 'hair', 'gaming', 'sitting', 'spend', 'temper', 'othermore']
+    
+    for ix, term in enumerate(terms):
+        if term in value:
+            return ix 
+
+        
+def selected_recode(df):
+    
+    df['SAGE_behavior_choices_1_bl_v1___selected_recode'] = df['SAGE_behavior_choices_1_bl_v1___selected'].apply(recode)
+    
+    return df
+
+
+if __name__ == '__main__':
+    main()
+    print('Done!')
 
